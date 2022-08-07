3a6b8217a51febe183ab82b0136bbcf80d88e71c
Charlie <69505003+charliedodds@users.noreply.github.com>
2022-08-07 12:10:56 +0100

    Add props and comment to button

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,5 +1,15 @@
-import { FC } from "react";
+import { FC, ReactNode } from "react";
 
-const Button: FC = () => <button>I am a button</button>;
+interface Props {
+  children: ReactNode;
+  onClick: () => void;
+}
+
+/**
+ * A button
+ */
+const Button: FC<Props> = ({ children, onClick }) => (
+  <button onClick={onClick}>{children}</button>
+);
 
 export default Button;
